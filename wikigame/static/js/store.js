be2c9a2d827f5c55cd531acc2ea9c50917c6340f
Martin Zackrisson <martin.zackrisson@fripost.org>
2022-02-06 17:00:05 +0100

    New game

--- wikigame/static/js/store.js
+++ wikigame/static/js/store.js
@@ -2,7 +2,7 @@ window.wikiStore = {
     _LANGUAGE: 'en',
     getLanguage: function getLanguage() { return this._LANGUAGE; },
 
-    _GAME_NAME: 'default',
+    _GAME_NAME: 'game: -42',
     getGameName: function getGameName() { return this._GAME_NAME; },
     setGameName: function setGameName(value) { this._GAME_NAME = value; },
 
