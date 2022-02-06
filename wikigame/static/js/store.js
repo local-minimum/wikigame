07ffeb5e3144b01bc8cc14ca5367188b00637bb2
Martin Zackrisson <martin.zackrisson@fripost.org>
2022-02-06 15:52:28 +0100

    oops

--- wikigame/static/js/store.js
+++ wikigame/static/js/store.js
@@ -1,8 +1,8 @@
 window.wikiStore = {
-    _LANGUAGE = 'en',
+    _LANGUAGE: 'en',
     getLanguage: function getLanguage() { return this._LANGUAGE; },
 
-    _GAME_NAME = 'default',
+    _GAME_NAME: 'default',
     getGameName: function getGameName() { return this._GAME_NAME; },
     setGameName: function setGameName(value) { this._GAME_NAME = value },
 };
